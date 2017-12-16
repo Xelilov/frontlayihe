4667b9e77ea4d789d922e0568d5ad1e3efae3cf2
xelilov <you@example.comxelilov-x@mail.ru>
2017-12-16 19:55:41 +0400

    frontLay2

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ $('ul.nav li.dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+
+var i=true;
+$(".srch").click(function(){
+	if(i==true){
+		$(this).find("button").find('i.fa').removeClass("fa-search").addClass("fa-times")
+		i=false;
+	}
+	else{
+		$(this).find("button").find('i.fa').removeClass("fa-times").addClass("fa-search")
+		i=true;
+	}
+})
+
+$(".shw").click(function(){
+	$(".accordMenu").css({
+		marginLeft:'0'
+	})
+	$(".accordMenu").toggle()
+})
